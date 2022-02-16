4274eed90cfbe0cc6a3c6854eb96056835f7886f
Myrik <myrik260138@tut.by>
2022-02-17 00:58:43 +0100

    add test

--- tests/test_bulk_reference.py
+++ tests/test_bulk_reference.py
@@ -70,7 +70,9 @@ async def test_global_id(raise_graphql):
     )
     assert not result.errors
     assert result.data == {
-        "_entities": [{"emailField": "identifier@email.com", "id": "VXNlcjppZGVudGlmaWVy"}]
+        "_entities": [
+            {"emailField": "identifier@email.com", "id": "VXNlcjppZGVudGlmaWVy"}
+        ]
     }
 
 
@@ -129,7 +131,75 @@ async def test_local_id(raise_graphql):
     )
     assert not result.errors
     assert result.data == {
-        "_entities": [{"emailField": "identifier@email.com", "id": "VXNlcjppZGVudGlmaWVy"}]
+        "_entities": [
+            {"emailField": "identifier@email.com", "id": "VXNlcjppZGVudGlmaWVy"}
+        ]
+    }
+
+
+@pytest.mark.asyncio
+async def test_changed_id(raise_graphql):
+    @key("id")
+    @key("email_field")
+    class User(ObjectType):
+        id = graphene.ID(required=True)
+        email_field = String()
+
+        class Meta:
+            interfaces = (relay.Node,)
+
+        def resolve_id(self, info):
+            return to_global_id(self.__class__.__name__, f"data_{self.id}")
+
+        @classmethod
+        def _resolve_reference_bulk(cls, model, info):
+            for field in info.parent_type.fields.values():
+                if hasattr(field.type, "name"):
+                    field_type_name = field.type.name
+                    if (
+                        field_type_name.endswith("Connection")
+                        and field_type_name[:-10] == info.context.representation
+                    ):
+                        return field.resolve(model, info)
+
+        @classmethod
+        def get_node(cls, info, id):
+            return User(id=id, email_field=f"{id}@email.com")
+
+    class UserConnection(Connection):
+        class Meta:
+            node = User
+
+        class Edge:
+            other = String()
+
+    class Query(ObjectType):
+        node = relay.Node.Field()
+        users = relay.ConnectionField(UserConnection)
+
+        def resolve_users(root, info):
+            return [User.get_node(info, id="identifier")]
+
+    schema = build_schema(query=Query)
+
+    result = await graphql(
+        schema.graphql_schema,
+        _query,
+        variable_values={
+            "representations": [
+                {"__typename": "User", "id": "VXNlcjpkYXRhX2lkZW50aWZpZXI="}
+            ]
+        },
+        context_value=Context,
+    )
+    assert not result.errors
+    assert result.data == {
+        "_entities": [
+            {
+                "emailField": "identifier@email.com",
+                "id": "VXNlcjpkYXRhX2lkZW50aWZpZXI=",
+            }
+        ]
     }
 
 
@@ -178,11 +248,15 @@ async def test_second_key(raise_graphql):
         schema.graphql_schema,
         _query,
         variable_values={
-            "representations": [{"__typename": "User", "emailField": "identifier@email.com"}]
+            "representations": [
+                {"__typename": "User", "emailField": "identifier@email.com"}
+            ]
         },
         context_value=Context,
     )
     assert not result.errors
     assert result.data == {
-        "_entities": [{"emailField": "identifier@email.com", "id": "VXNlcjppZGVudGlmaWVy"}]
+        "_entities": [
+            {"emailField": "identifier@email.com", "id": "VXNlcjppZGVudGlmaWVy"}
+        ]
     }
